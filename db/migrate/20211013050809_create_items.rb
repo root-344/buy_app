0eb8e0a16ddff101a2adf0a4989bd681a257f4e9
root-344 <michimasa8@gmail.com>
2021-10-13 14:28:51 +0900

    create item model

---
+++ db/migrate/20211013050809_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :product
+      t.string :farm
+      t.text :description
+      t.integer :states_id
+      t.integer :shiipping_fee_id
+      t.integer :region_id
+      t.integer :eta_id
+      t.integer :price
+      t.timestamps
+    end
+  end
+end
