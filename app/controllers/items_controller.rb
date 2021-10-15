2586ecf9032707fd00b3a96f2707ca02ee885629
root-344 <michimasa8@gmail.com>
2021-10-16 02:54:21 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,24 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def new
+    @item = Item.new    
+  end
+
+  def create
+    Item.create(item_params)  
+    redirect_to root_path 
+  end
+
 private
+
+  def item_params
+    params.require(:item).permit(:product, :description, :category_id, 
+                                 :states_id, :shipping_fee_id, :region_id, :eta_id, :price).merge(farm_id: current_farm.id)
+  end
+
   def move_to_index
-    unless user_signed_in?
+    unless farm_signed_in?
       redirect_to action: :index
     end
   end
