e23c9de0f51d2ee78a060584d98371e380cb3228
root-344 <michimasa8@gmail.com>
2021-10-17 05:11:39 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,14 @@ class ItemsController < ApplicationController
       render :edit
     end   
   end
+  
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+      redirect_to root_path
+    end
+  end
+
 
 
 private
