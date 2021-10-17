c2df1c6ec6ff32e877ce5ad79956fe6e84b17a6e
root-344 <michimasa8@gmail.com>
2021-10-18 01:21:18 +0900

    Create script.js

---
+++ app/javascript/script.js
@@ -0,0 +1,58 @@
+window.addEventListener('load', function(){
+
+  const farmPullDownButton = document.getElementById("farm-select")
+  const farmPullDownBox = document.getElementById("farm-pull-down")
+  const storePullDownButton = document.getElementById("store-select")
+  const storePullDownBox = document.getElementById("store-pull-down")
+
+  farmPullDownButton.addEventListener('mouseover', function() {
+    this.setAttribute("style", "color:red;")
+  });
+
+  farmPullDownButton.addEventListener('mouseout', function() {
+    this.removeAttribute("style", "color:red;")
+  });
+
+  storePullDownButton.addEventListener('mouseover', function() {
+    this.setAttribute("style", "color:red;")
+  });
+
+  storePullDownButton.addEventListener('mouseout', function() {
+    this.removeAttribute("style", "color:red;")
+  });
+
+
+  farmPullDownButton.addEventListener('click', function() {
+    if (farmPullDownBox.getAttribute("style") == "display:block") {
+      farmPullDownBox.removeAttribute("style", "display:block")
+    } else {
+    farmPullDownBox.setAttribute("style", "display:block");
+    }
+  });
+
+  storePullDownButton.addEventListener('click', function() {
+    if (storePullDownBox.getAttribute("style") == "display:block") {
+      storePullDownBox.removeAttribute("style", "display:block")
+    } else {
+      storePullDownBox.setAttribute("style", "display:block");
+    }
+  });
+
+  // secondFarmPullDownButton.addEventListener('click', function() {
+  //   if (secondFarmPullDownBox.getAttribute("style") == "display:block") {
+  //     secondFarmPullDownBox.removeAttribute("style", "display:block")
+  //   } else {
+  //   secondFarmPullDownBox.setAttribute("style", "display:block");
+  //   }
+  // });
+
+  // secondStorePullDownButton.addEventListener('click', function() {
+  //   if (secondStorePullDownBox.getAttribute("style") == "display:block") {
+  //     secondStorePullDownBox.removeAttribute("style", "display:block")
+  //   } else {
+  //     secondStorePullDownBox.setAttribute("style", "display:block");
+  //   }
+  // });
+
+
+});
