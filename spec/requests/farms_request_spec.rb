fb932b2016be6b67676890d87e352e5bd0582a2f
root-344 <michimasa8@gmail.com>
2021-10-21 23:14:14 +0900

    play rubocop

--- spec/requests/farms_request_spec.rb
+++ spec/requests/farms_request_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Farms", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/farms/index"
+RSpec.describe 'Farms', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/farms/index'
       expect(response).to have_http_status(:success)
     end
   end
