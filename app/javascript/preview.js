11fd1d0d0cd416f70f976b7794509f16a6e0f062
root-344 <michimasa8@gmail.com>
2021-10-25 02:14:55 +0900

    Update preview.js

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,7 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
 document.addEventListener('DOMContentLoaded', function() {
   const ImageList = document.getElementById('image-list');
-
+  
   const createImageHTML = (blob) => {
     const imageElement = document.createElement('div');
     imageElement.setAttribute('class', "image-element");
@@ -9,6 +10,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const blobImage = document.createElement('img');
     blobImage.setAttribute('src', blob);
 
+      
+    if(imageElementNum<2){
     const inputHTML = document.createElement('input');
     inputHTML.setAttribute('id',  `item_image_${imageElementNum}`);
     inputHTML.setAttribute('name','item[images][]');
@@ -17,24 +20,31 @@ document.addEventListener('DOMContentLoaded', function() {
     imageElement.appendChild(blobImage);
     imageElement.appendChild(inputHTML);
     ImageList.appendChild(imageElement);
-
+  
     inputHTML.addEventListener('change', (e) => {
       file = e.target.files[0];
       blob = window.URL.createObjectURL(file);
-
       createImageHTML(blob);
     });
+  } else if(imageElementNum == 2) {
+    imageElement.appendChild(blobImage);
+    ImageList.appendChild(imageElement);
+  } else {
+      const imageContent = document.querySelector("img")
+      if(imageContent){  
+      imageContent.remove();  
+      }  
+
+    };
   };
- 
-  if (!document.getElementById('item-image')){return false;}
   document.getElementById('item-image').addEventListener('change', function(e) {
-    // const imageContent = document.querySelector('img');
-    // if (imageContent) {
-    //   imageContent.remove();
-    // }
+      // const imageContent = document.querySelector("img")
+      // if(imageContent){  
+      // imageContent.remove();  
+      // }  
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
       createImageHTML(blob);
   });
 });
-
+};
