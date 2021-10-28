e9b0bf22f384585534c9c2c88150f47384b02d96
root-344 <michimasa8@gmail.com>
2021-10-28 14:35:12 +0900

    Update stores_controller.rb

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,8 +1,20 @@
 class StoresController < ApplicationController
+  before_action :move_to_login
+  before_action :set_item
   def show
     store = Store.find(params[:id])
     @nickname = store.nickname
     @orders = store.orders
+  end
+
+  private
+  def set_item
     @item = Item.find(params[:id])
   end
+
+  def move_to_login
+    unless store_signed_in?
+      redirect_to store_session_path 
+    end
+  end  
 end
