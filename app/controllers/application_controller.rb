43bc39131290cdd213a212a354716c326104b0af
root-344 <michimasa8@gmail.com>
2021-10-28 14:34:07 +0900

    Create before_action set_side

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_side
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana])
+  end
+
+  def set_side
+    @farms = Farm.all
   end
 end
