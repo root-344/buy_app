c5ac8ef636df00b21df8e5331d636d83d9ad0cd6
root-344 <michimasa8@gmail.com>
2021-11-04 00:04:38 +0900

    Update farms_controller.rb

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,8 +1,10 @@
 class FarmsController < ApplicationController
+
   def show
     farm = Farm.find(params[:id])
     @nickname = farm.nickname
     @email = farm.email
     @items = farm.items
+    @item = Item.find_by(params[:farm_id])
   end
 end
