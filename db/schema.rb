f52529a409b173a0eb39be04e07eee6d190b57f8
root-344 <michimasa8@gmail.com>
2021-11-19 10:51:58 +0900

    モデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_131646) do
+ActiveRecord::Schema.define(version: 2021_11_19_013649) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -78,6 +78,15 @@ ActiveRecord::Schema.define(version: 2021_10_22_131646) do
     t.index ["farm_id"], name: "index_items_on_farm_id"
   end
 
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "store_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_likes_on_item_id"
+    t.index ["store_id"], name: "index_likes_on_store_id"
+  end
+
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "store_id", null: false
     t.bigint "item_id", null: false
@@ -108,6 +117,8 @@ ActiveRecord::Schema.define(version: 2021_10_22_131646) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "orders"
   add_foreign_key "items", "farms"
+  add_foreign_key "likes", "items"
+  add_foreign_key "likes", "stores"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "stores"
 end
