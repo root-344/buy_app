444534a671804aef485fd8523edf9889c877ef8f
root-344 <michimasa8@gmail.com>
2021-12-03 15:36:41 +0900

    rubocop実行

--- spec/factories/stores.rb
+++ spec/factories/stores.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :store do
     nickname { 'test' }
     email { Faker::Internet.unique.free_email }
-    password { 'a1' + Faker::Internet.password(min_length: 6) }
+    password { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation { password }
     gimei = Gimei.name
     last_name { gimei.last.kanji }
@@ -13,6 +13,5 @@ FactoryBot.define do
     after(:build) do |store|
       store.image.attach(io: File.open('public/images/image.png'), filename: 'image.png')
     end
-
   end
 end
