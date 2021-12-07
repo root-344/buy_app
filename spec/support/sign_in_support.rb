61238e03192e2f013e1d5caafd86847256735ef8
root-344 <michimasa8@gmail.com>
2021-12-07 22:11:42 +0900

    購入者ログインモジュール作成

--- spec/support/sign_in_support.rb
+++ spec/support/sign_in_support.rb
@@ -1,4 +1,5 @@
 module SignInSupport
+  # 出品者
   def sign_in(farm)
       #トップページに移動する
       visit items_path
@@ -13,10 +14,22 @@ module SignInSupport
       end.to change { Farm.count }.by(0)
       # トップページに移動した事を確認する
       expect(current_path).to eq(items_path)
-    # visit items_path
-    # fill_in 'farm_email', with: @farm.email
-    # fill_in 'farm_password', with: @farm.password
-    # click_on('ログイン')
-    # expect(current_path).to eq(items_path)
+  end
+  
+  # 購入者
+  def sign_in(store)
+    #トップページに移動する
+    visit items_path
+    # ログインページに移動する
+    visit new_store_session_path
+    # 出品者情報を入力する
+    fill_in 'store_email', with: @store.email
+    fill_in 'store_password', with: @store.password
+    # ログインボタンを押してもstoreテーブルにレコード追加されない事を確認する
+    expect  do
+      find('input[value="ログイン"]').click
+    end.to change { Store.count }.by(0)
+    # トップページに移動した事を確認する
+    expect(current_path).to eq(items_path)
   end
 end
